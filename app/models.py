581590a57dd0c2c5994b0167e4cd09d6900eb222
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-09 15:42:35 +0200

    models for db

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
 from __init__ import db
+
+
+class Users(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(16), index=True,
+                         unique=True, nullable=False)
+    email = db.Column(db.String(64), index=False, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User {self.username}"
