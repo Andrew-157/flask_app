d474310a245dfafb47cb39dbc42a653ae42a1186
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-09 16:50:17 +0200

    migration registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config):
@@ -11,6 +13,7 @@ def create_app(config):
     app.config.from_object(config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
 
