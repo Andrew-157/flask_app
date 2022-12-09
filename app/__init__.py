eb67242545ef0efc02531e1286789cd694897221
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-09 15:17:19 +0200

    config object

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
 
-def create_app():
+def create_app(config):
 
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config)
 
     return app
